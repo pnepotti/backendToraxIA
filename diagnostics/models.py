e488fc5571699a17735040c4f6d45fbd837eede6
Paulo Nepotti <pnepotti@gmail.com>
2024-10-04 23:59:09 -0300

    cambia de base de datos a PostgreSQL

--- diagnostics/models.py
+++ diagnostics/models.py
@@ -1,16 +1,24 @@
 from django.db import models
-"""
-    Modelo para almacenar información sobre radiografías"""
 
 
 class Radiography(models.Model):
+    # Información del paciente
     patient_name = models.CharField(max_length=100)
     patient_dni = models.CharField(max_length=10)
+
+    # Información del médico
     doctor_name = models.CharField(max_length=100)
+
+    # Información del diagnóstico
+    disease = models.CharField(max_length=100, default="Unknown")
+    prediccion_probabilidad = models.FloatField()
+    prediccion_confianza = models.FloatField()
+    prediccion_entropia = models.FloatField()
+    diagnosed_at = models.DateTimeField(
+        auto_now_add=True)  # Fecha automática al guardar
+
+    # Imagen de la radiografía
     image = models.ImageField(upload_to='radiographies/')
-    # Agrega campo de fecha automática
-    # uploaded_at = models.DateTimeField(auto_now_add=True)
-    desease = models.CharField(max_length=100, default="Unknown")
 
     def __str__(self):
-        return f"Radiografía de {self.patient_name}"
+        return f"Radiografía de {self.patient_name} (Diagnóstico: {self.disease})"
